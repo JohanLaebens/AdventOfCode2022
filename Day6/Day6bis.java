32fa2628be15dc918e116030fc7f7693b5b2f6bd
wn5874 <j@j.com>
2022-12-06 15:48:33 +0100

    Day 6Bis - Optimalisation for Day6b

---
+++ Day6/Day6bis.java
@@ -0,0 +1,66 @@
+package Day6;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Day6bis {
+
+    List<String> allLines;
+
+    public static void main(String[] args) {
+        new Day6bis();
+    }
+
+    Day6bis() {
+        // same as Day6b but a bit optimised
+
+
+        readfile("input");
+
+        String input = "";
+        input = allLines.get(0);
+      //  input = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+
+        int messageLength = 14;
+        int start = 0;
+
+        String[] temp = new String[messageLength];
+        boolean resultFound = false;
+
+        while (resultFound == false)
+        {
+            String workString = input.substring(start,start+messageLength);
+
+            checkForLoop:
+            for (int i = 0;i<=messageLength-1;i++)
+            {
+                temp[i] = workString.replace(workString.substring(i,i+1),"");
+                if (temp[i].length() == messageLength-1)
+                {
+                    if (i==messageLength-1) // we ran through the entire message and found no duplicates
+                    {
+                        resultFound=true; // so we can escape the while
+                    }
+                }
+                else
+                {
+                    start++; // move on to the next
+                    break checkForLoop;
+                }
+            }
+        }
+        System.out.println(start+messageLength);
+    }
+
+    private  void readfile(String filename)
+    {
+        try {
+            System.out.println(Paths.get(Paths.get(".").toAbsolutePath().normalize().toString() ));
+            allLines = Files.readAllLines(Paths.get(Paths.get(".").toAbsolutePath().normalize().toString() + "\\Day6\\" + filename +".txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
